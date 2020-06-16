87b3a9e13dcc744a722194d1d851c03cd9d64987
st-quando <quan.do@supremetech.vn>
2020-06-16 15:53:04 +0700

    redeploy

--- ops/run-server.sh
+++ ops/run-server.sh
@@ -1,10 +1,6 @@
 NC='\033[0m'
 GREEN='\033[0;32m'
-npm install
-npm install fibers
-printf "${GREEN}Build Packages successful!${NC}\n"
-npm run build
-printf "${GREEN}Build Code successful!${NC}\n"
+
 rm -rf tmpfile
 rm -rf ssmtmpfile
 printf "${GREEN}Clean uneccessary files!${NC}\n"
